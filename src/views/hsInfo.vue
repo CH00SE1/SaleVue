c28fa895409e7c3bb9507f6d5a137e419983f4a6
liushaoxiong <3354621952@qq.com>
2022-08-03 14:16:07 +0800

    修改hsInfo查询条件

--- src/views/hsInfo.vue
+++ src/views/hsInfo.vue
@@ -2,16 +2,33 @@
   <el-container style="height: 800px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
       <el-menu :default-openeds="['1', '3']" :router="true" :disabled="true">
-        <el-button type="text" @click="openTitle">查询标题</el-button>
+        <!-- <el-button type="text" @click="openTitle">查询标题</el-button> -->
+        <el-button type="text" @click="dialogFormVisible = true">打开查询条件</el-button>
+        <el-dialog title="查询信息" :visible.sync="dialogFormVisible">
+          <el-form :model="queryParams">
+            <el-form-item label="标题" :label-width="formLabelWidth">
+              <el-input v-model="queryParams.title" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="平台" :label-width="formLabelWidth">
+              <el-input v-model="queryParams.platform" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="openTitle">确 定</el-button>
+          </div>
+        </el-dialog>
       </el-menu>
     </el-aside>
     <el-container>
       <el-header style="text-align: center; font-size: 30px">
-      <span>{{ queryParams.title }}</span>
+        <span>{{ queryParams.title }} * {{ queryParams.platform }}</span>
       </el-header>
       <el-main>
         <template>
-            <el-table :data="hsInfoList" border style="width: 150%" :row-style="{height: '0'}" :cell-style="{padding: '3px'}" :header-cell-style="{ background: '#eef1f6', color: '#606266' }" :default-sort="{prop: 'sum_fl_money', order: 'descending'}">
+          <el-table :data="hsInfoList" style="width: 150%" :row-style="{ height: '0' }" :cell-style="{ padding: '3px' }"
+            :header-cell-style="{ background: '#eef1f6', color: '#606266' }"
+            :default-sort="{ prop: 'sum_fl_money', order: 'descending' }">
             <el-table-column prop="id" label="序号" width="140"></el-table-column>
             <el-table-column prop="createdAt" label="创建时间" width="120"></el-table-column>
             <el-table-column prop="title" label="标题"> </el-table-column>
@@ -21,31 +38,20 @@
             <el-table-column prop="platform" label="平台"> </el-table-column>
             <el-table-column prop="page" label="页码"> </el-table-column>
             <el-table-column prop="location" label="位置"> </el-table-column>
-              <el-table-column
-                fixed="right"
-                label="操作"
-                width="120">
-                <template slot-scope="scope">
-                <el-button
-                @click.native.prevent="downloadRow(scope.$index)"
-                type="text"
-                size="small">
-                下载
+            <el-table-column fixed="right" label="操作" width="120">
+              <template slot-scope="scope">
+                <el-button @click.native.prevent="downloadRow(scope.$index)" type="text" size="small">
+                  下载
                 </el-button>
-                </template>
-             </el-table-column>
-        </el-table>
-        <div class="pagination">
-        <el-pagination
-        background
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :page-sizes="[3, 5, 10, 15, 20, 30, 50, 100]"
-        :page-size="queryParams.pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total">
-        </el-pagination>
-        </div>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="pagination">
+            <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+              :page-sizes="[3, 5, 10, 15, 20, 30, 50, 100]" :page-size="queryParams.pageSize"
+              layout="total, sizes, prev, pager, next, jumper" :total="total">
+            </el-pagination>
+          </div>
         </template>
       </el-main>
     </el-container>
@@ -75,22 +81,32 @@ export default {
     return {
       hsInfoList: [],
       total: null,
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      formLabelWidth: '120px',
       // 查询参数
       queryParams: {
         title: null,
         pageNum: 1,
-        pageSize: 5
+        pageSize: 5,
+        platform: null
       }
     }
   },
   created () {
-    this.openTitle()
+    // this.openTitle()
   },
   methods: {
     getList () {
       listHsInfo(this.queryParams).then((_result) => {
         this.hsInfoList = _result.data.data.rows
         this.total = _result.data.data.total
+        this.$notify({
+          title: '请求消息',
+          message: this.queryParams,
+          offset: 100,
+          type: 'success'
+        })
       }).catch((_err) => {
         this.$message({
           showClose: true,
@@ -100,22 +116,8 @@ export default {
       })
     },
     openTitle () {
-      this.$prompt('请输入标题', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消'
-      }).then(({ value }) => {
-        this.$message({
-          type: 'success',
-          message: '你的标题是: ' + value
-        })
-        this.queryParams.title = value
-        this.getList()
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '取消输入'
-        })
-      })
+      this.getList()
+      this.dialogFormVisible = false
     },
     handleSizeChange (val) {
       // 更新每页条数
