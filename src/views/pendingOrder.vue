b4f7e7b51fe46829db8abf89bf20119d4ba00639
liushaoxiong <3354621952@qq.com>
2022-09-06 15:13:39 +0800

    修改挂单页面

--- src/views/pendingOrder.vue
+++ src/views/pendingOrder.vue
@@ -5,14 +5,15 @@
       </el-input>
     </div>
     <div id="selectShopId">
-          <el-form :model="queryParams">
-            <template>
-              <el-select v-model="queryParams.placepointid" placeholder="选择门店">
-                <el-option v-for="item in options" :key="item.placepointid" :label="item.placepointname" :value="item.placepointid">
-                </el-option>
-              </el-select>
-            </template>
-          </el-form>
+      <el-form :model="queryParams">
+        <template>
+          <el-select v-model="queryParams.placepointid" placeholder="选择门店">
+            <el-option v-for="item in options" :key="item.placepointname" :label="item.placepointname"
+              :value="item.placepointid">
+            </el-option>
+          </el-select>
+        </template>
+      </el-form>
     </div>
     <div id="search">
       <el-button type="primary" icon="el-icon-search" v-on:click="handleQuery">查询</el-button>
@@ -35,20 +36,42 @@
         <span>零售挂单信息</span>
       </el-header>
       <el-main>
-        <el-table :data="pengdingOrders" stripe style="width: 100%">
-          <el-table-column prop="rsaid" label="总单ID" width="120">
+        <el-table :data="pengdingOrders" stripe style="width: 100%"
+          :default-sort="{ prop: 'date', order: 'descending' }">
+          <el-table-column prop="rsaid" label="总单ID" sortable width="120">
           </el-table-column>
-          <el-table-column prop="rsadtlid" label="细单ID" width="120">
+          <el-table-column prop="rsadtlid" label="细单ID" sortable width="120">
           </el-table-column>
-          <el-table-column prop="credate" label="创建时间" width="180">
+          <el-table-column prop="credate" label="创建时间" sortable width="180">
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.credate }}</span>
+            </template>
           </el-table-column>
           <el-table-column prop="placepointname" label="门店名称" width="180">
           </el-table-column>
           <el-table-column prop="cashiername" label="调出挂单人">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>人员工号:{{ scope.row.cashierid }}</p>
+                <p>门店ID:{{ scope.row.placepointid }}</p>
+                <div slot="reference" class="name-wrapper">
+                  <el-tag size="medium">{{ scope.row.cashiername }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
           </el-table-column>
           <el-table-column prop="clerkername" label="营业员">
+            <template slot-scope="scope">
+              <el-popover trigger="hover" placement="top">
+                <p>人员工号:{{ scope.row.clerkerid }}</p>
+                <div slot="reference" class="name-wrapper">
+                  <el-tag type="success">{{ scope.row.clerkername }}</el-tag>
+                </div>
+              </el-popover>
+            </template>
           </el-table-column>
-          <el-table-column prop="realmoney" label="实收金额">
+          <el-table-column prop="realmoney" sortable label="实收金额">
           </el-table-column>
           <el-table-column prop="goodsid" label="货品ID">
           </el-table-column>
@@ -56,7 +79,7 @@
           </el-table-column>
           <el-table-column prop="goodsunit" label="货品单位">
           </el-table-column>
-          <el-table-column prop="goodsqty" label="货品数量">
+          <el-table-column prop="goodsqty" sortable label="货品数量">
           </el-table-column>
           <el-table-column prop="useprice" label="货品单价">
           </el-table-column>
@@ -64,57 +87,72 @@
           </el-table-column>
           <el-table-column prop="factoryname" label="货品厂家">
           </el-table-column>
+          <el-table-column fixed="right" label="操作" width="90">
+            <el-button @click.native.prevent="exportExcel('.el-table__fixed-right', '挂单数据')"
+              @click="dialogTableVisible = true" type="danger" size="mini">
+              导出
+            </el-button>
+          </el-table-column>
         </el-table>
         <div class="pagination">
-            <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
-              :page-sizes="[5, 7, 10, 20, 50]" :page-size="queryParams.pageSize"
-              layout="total, sizes, prev, pager, next, jumper" :total="total">
-            </el-pagination>
-          </div>
+          <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+            :page-sizes="[5, 7, 10, 20, 50, 100, 200, 1000]" :page-size="queryParams.pageSize"
+            layout="total, sizes, prev, pager, next, jumper" :total="total">
+          </el-pagination>
+        </div>
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <style scoped>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  .el-aside {
-    color: #333;
-  }
-  #search-shopId {
-    position: absolute;
-    top: 1.5%;
-    left: 24%;
-  }
-  #selectShopId {
-    position: absolute;
-    top: 1.5%;
-    left: 36%;
-  }
-  #search {
-    position: absolute;
-    top: 1.5%;
-    left: 49%;
-  }
-  #reset {
-    position: absolute;
-    top: 1.5%;
-    left: 55%;
-  }
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+
+#search-shopId {
+  position: absolute;
+  top: 1.5%;
+  left: 24%;
+}
+
+#selectShopId {
+  position: absolute;
+  top: 1.5%;
+  left: 36%;
+}
+
+#search {
+  position: absolute;
+  top: 1.5%;
+  left: 49%;
+}
+
+#reset {
+  position: absolute;
+  top: 1.5%;
+  left: 55%;
+}
 </style>
 
 <script>
 import { pendingOrder, shopInfoAll } from '../api/index'
+import * as XLSX from 'xlsx'
+import FileSaver from 'file-saver'
+
 export default {
   data () {
     return {
       pengdingOrders: [],
       total: null,
       options: [],
+      dialogTableVisible: false,
       queryParams: {
         pageNum: 1,
         pageSize: 5,
@@ -127,13 +165,23 @@ export default {
     this.getShopInfo()
   },
   methods: {
+    // 导出excel表格
+    exportExcel (val, fliename) {
+      let table = document.querySelector(val).cloneNode(true)
+      var wb = XLSX.utils.table_to_book(table, { raw: true })
+      var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' })
+      try {
+        FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), fliename + '.xlsx')
+      } catch (e) { if (typeof console !== 'undefined') console.log(e, wbout) }
+      return wbout
+    },
     getList () {
       pendingOrder(this.queryParams).then((_result) => {
         if (_result.data.code === 200) {
           if (_result.data.msg === '0') {
             this.$notify({
               title: '门店ID' + this.queryParams.placepointid,
-              message: '没有挂单记录',
+              message: '没有查询到挂单',
               type: 'success',
               position: 'bottom-left'
             })
